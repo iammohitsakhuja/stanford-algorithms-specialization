f95a9fe4ff85f4bc3cb5e8c32ebf60cb421dadd8
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-07-16 15:37:49 +0530

    Supply mod argument to rand via CLI

--- part-1/week-2/rand.c
+++ part-1/week-2/rand.c
@@ -4,18 +4,19 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        printf("Usage: %s num\n", argv[0]);
+        printf("Usage: %s num mod\n", argv[0]);
         return 1;
     }
 
     srand(time(NULL));
 
     int num = atoi(argv[1]);
+    int mod = atoi(argv[2]);
 
     for (int i = 0; i < num; i++)
-        printf("%i\n", rand() % 10000);
+        printf("%i\n", rand() % mod);
 
     return 0;
 }
