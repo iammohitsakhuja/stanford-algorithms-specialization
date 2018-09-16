ef354945888cf91e96140ff52efdd9c0b8e3c880
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-09-16 11:43:57 +0530

    Add benchmarks to Huffman coding

--- part-3/week-3/HuffmanCoding/HuffmanCoding.java
+++ part-3/week-3/HuffmanCoding/HuffmanCoding.java
@@ -36,21 +36,47 @@ class HuffmanCoding {
     // Get the inputs.
     String fileName = args[0];
 
-    // Load weights from the given file.
+    // Benchmarks.
+    double timeLoadingWeights = 0.0;
+    double timeBuildingTreeFromQueue = 0.0;
+    double timeBuildingTreeFromHeap = 0.0;
+
+    // Load weights from the given file and benchmark the time taken.
+    long startTime = System.nanoTime();
     int[] weights = loadWeightsFromFile(fileName);
+    timeLoadingWeights = (System.nanoTime() - startTime) / 1000000.0;
     if (weights == null) {
       System.err.println("Could not load weights");
       System.exit(2);
     }
 
-    // Build Huffman tree.
+    // Build Huffman tree by using 2 queues and benchmark the time taken.
+    startTime = System.nanoTime();
     Tree huffmanTreeFromQueue = new Tree(weights);
+    timeBuildingTreeFromQueue = (System.nanoTime() - startTime) / 1000000.0;
+
+    // Build Huffman tree by using a heap and benchmark the time taken.
+    startTime = System.nanoTime();
     Tree huffmanTreeFromHeap = new Tree(weights, true);
+    timeBuildingTreeFromHeap = (System.nanoTime() - startTime) / 1000000.0;
 
-    System.out.println(huffmanTreeFromQueue.getMaximumLength());
-    System.out.println(huffmanTreeFromQueue.getMinimumLength());
+    // Display the Maximum and Minimum Length of codewords.
+    System.out.printf(
+        "Maximum length of codeword (queue): %2d\n", huffmanTreeFromQueue.getMaximumLength());
+    System.out.printf(
+        "Minimum length of codeword (queue): %2d\n\n", huffmanTreeFromQueue.getMinimumLength());
 
-    System.out.println(huffmanTreeFromHeap.getMaximumLength());
-    System.out.println(huffmanTreeFromHeap.getMinimumLength());
+    System.out.printf(
+        "Maximum length of codeword (heap): %3d\n", huffmanTreeFromHeap.getMaximumLength());
+    System.out.printf(
+        "Minimum length of codeword (heap): %3d\n\n", huffmanTreeFromHeap.getMinimumLength());
+
+    // Display the benchmark results.
+    System.out.printf("TIME IN loading weights:             %6.2fms\n", timeLoadingWeights);
+    System.out.printf("TIME IN building tree from 2 queues: %6.2fms\n", timeBuildingTreeFromQueue);
+    System.out.printf("TIME IN building tree from heap:     %6.2fms\n", timeBuildingTreeFromHeap);
+    System.out.printf(
+        "TIME IN total:                       %6.2fms\n",
+        timeLoadingWeights + timeBuildingTreeFromQueue + timeBuildingTreeFromHeap);
   }
 }
